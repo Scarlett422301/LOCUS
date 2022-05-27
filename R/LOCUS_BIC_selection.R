00e75c67822ea2439e4a60dbb1ac04db924b2bbb
scarlett <jialu.ran@emory.edu>
2022-05-27 17:49:10 -0400

    update bic related functions

--- R/LOCUS_BIC_selection.R
+++ R/LOCUS_BIC_selection.R
@@ -1,6 +1,7 @@
 # Use BIC method to select the hyper-parameters of phi and rho. 
 LOCUS_BIC_selection <- function(Y, q, V, MaxIteration=50, penalty="SCAD", phi_grid_search=seq(0.2, 1, 0.2), 
-                      rho_grid_search=c(0.95), espli1=0.001, espli2=0.001, save_LOCUS_output=T)
+                      rho_grid_search=c(0.95), espli1=0.001, espli2=0.001, save_LOCUS_output=T, 
+                      preprocess = T, demean = T)
   # Y: connectivity data of dimension N x K, N is number of subjects, K is number of edges. 
   # q: Number of subnetworks to extract.
   # V: Number of nodes in network.
@@ -11,23 +12,26 @@ LOCUS_BIC_selection <- function(Y, q, V, MaxIteration=50, penalty="SCAD", phi_gr
   # rho_grid_search: grid search candidates of tuning parameter for selecting number of ranks in each subnetwork's decomposition. 
 {
   
-  # preprocess Y to speed up 
-  ## Yraw = Y
-  Y_demean = sweep(Y,2,apply(Y,2,mean),"-") 
-  Y_preprocess = Locus_preprocess(Y_demean, q)
+  # demean or preprocess Y to speed up 
+  if(demean){
+    Y = sweep(Y,2,apply(Y,2,mean),"-") 
+  }
+  if(preprocess){
+    Y = Locus_preprocess(Y, q)
+  }
   
   # run grid search  
   LOCUS_results = list()
-  bic_tab = matrix(0, ncol=3, nrow=length(rho_grid_search) * length(phi_grid_search))
+  bic_tab = matrix(0, ncol = 3, nrow=length(rho_grid_search) * length(phi_grid_search))
   k = 1
   for(rho in rho_grid_search)
   {
     for(phi in phi_grid_search)
     {
       print(paste0('Running LOCUS for rho=', rho, ', phi=', phi, '...'))
-      LOCUS_output = LOCUS(Y_preprocess, q=q, V=V, MaxIteration=MaxIteration, penalty=penalty, phi=phi, approximation=T, 
+      LOCUS_output = LOCUS(Y, q=q, V=V, MaxIteration=MaxIteration, penalty=penalty, phi=phi, approximation=T, 
                            preprocess=F, espli1=espli1, espli2=espli1, rho=rho, silent=T, demean=F)
-      bic_value = calculate_bic(Y_preprocess, LOCUS_output$A, LOCUS_output$S)
+      bic_value = calculate_bic(Y, LOCUS_output$A, LOCUS_output$S)
       bic_tab[k,] = c(rho, phi, bic_value)
       if(save_LOCUS_output)
       {
