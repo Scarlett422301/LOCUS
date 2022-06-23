f173939da7019e9a01927f627b12c314475e48de
Jialu Ran <jialu.ran@emory.edu>
2022-06-22 22:01:52 -0400

    .Rd file, main function, bic function update
    update .Rd file, delete demean option in LOCUS main function, update the bic example

--- R/LOCUS_BIC_selection.R
+++ R/LOCUS_BIC_selection.R
@@ -1,21 +1,19 @@
 # Use BIC method to select the hyper-parameters of phi and rho. 
 LOCUS_BIC_selection <- function(Y, q, V, MaxIteration=50, penalty="SCAD", phi_grid_search=seq(0.2, 1, 0.2), 
                       rho_grid_search=c(0.95), espli1=0.001, espli2=0.001, save_LOCUS_output=T, 
-                      preprocess = T, demean = T)
+                      preprocess = T)
   # Y: connectivity data of dimension N x K, N is number of subjects, K is number of edges. 
   # q: Number of subnetworks to extract.
   # V: Number of nodes in network.
   # MaxIteration = 100: number of maximum iterations.
   # espli1=0.001, espli2=0.001: toleration for convergence on S and A. 
-  # penalty = "SCAD": sparsity penalty, this can be NULL, SCAD, L1 or Hardthreshold. 
+  # penalty = "SCAD": sparsity penalty, this can be NULL, SCAD, or L1.
   # phi_grid_search: grid search candidates of tuning parameter for penalty
   # rho_grid_search: grid search candidates of tuning parameter for selecting number of ranks in each subnetwork's decomposition. 
 {
   
   # demean or preprocess Y to speed up 
-  if(demean){
-    Y = sweep(Y,2,apply(Y,2,mean),"-") 
-  }
+  Y = sweep(Y,2,apply(Y,2,mean),"-") 
   if(preprocess){
     Y = Locus_preprocess(Y, q)
   }
@@ -29,7 +27,7 @@ LOCUS_BIC_selection <- function(Y, q, V, MaxIteration=50, penalty="SCAD", phi_gr
     for(phi in phi_grid_search)
     {
       print(paste0('Running LOCUS for rho=', rho, ', phi=', phi, '...'))
-      LOCUS_output = LOCUS(Y, q=q, V=V, MaxIteration=MaxIteration, penalty=penalty, phi=phi, approximation=T, 
+      LOCUS_output = LOCUS_temp(Y, q=q, V=V, MaxIteration=MaxIteration, penalty=penalty, phi=phi, approximation=T, 
                            preprocess=F, espli1=espli1, espli2=espli1, rho=rho, silent=T, demean=F)
       bic_value = calculate_bic(Y, LOCUS_output$A, LOCUS_output$S)
       bic_tab[k,] = c(rho, phi, bic_value)
@@ -44,6 +42,7 @@ LOCUS_BIC_selection <- function(Y, q, V, MaxIteration=50, penalty="SCAD", phi_gr
   
   if(save_LOCUS_output)
   {
+    colnames(bic_tab) = c("rho", "phi", "bic_value")
     return(list(bic_tab = bic_tab, LOCUS_results = LOCUS_results))
   }
   return(list(bic_tab = bic_tab))
